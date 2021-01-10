c1105b33332d173ba715123b322e978e5287529c
DavidLukacs <lukacs.dvid@gmail.com>
2021-01-10 21:36:54 +0100

    categories working 100 percent

--- app/Http/Controllers/AdminCategoriesController.php
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -73,6 +73,11 @@ class AdminCategoriesController extends Controller
     public function edit($id)
     {
         //
+
+        $category = Category::findOrFail($id);
+
+        return view('admin.categories.edit', compact('category'));
+
     }
 
     /**
@@ -85,6 +90,13 @@ class AdminCategoriesController extends Controller
     public function update(Request $request, $id)
     {
         //
+
+        $category = Category::findOrFail($id);
+
+        $category->update($request->all());
+
+        return redirect('/admin/categories');
+
     }
 
     /**
@@ -96,5 +108,10 @@ class AdminCategoriesController extends Controller
     public function destroy($id)
     {
         //
+
+        Category::findOrFail($id)->delete();
+
+        return redirect('/admin/categories');
+
     }
 }
--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -8,7 +8,7 @@
     <div class="col-sm-6">
 
 
-        {!! Form::model($category, ['method'=>'PATCH', 'action'=>'AdminCategoriesController@update', $category->id]) !!}
+        {!! Form::model($category, ['method'=>'PATCH', 'action'=> ['AdminCategoriesController@update', $category->id]]) !!}
 
         <div class="form-group">
             {!! Form::label('name', 'Name:') !!}
@@ -16,7 +16,19 @@
         </div>
 
         <div class="form-group">
-            {!! Form::submit('Update Category', ['class'=>'btn btn-primary']) !!}
+            {!! Form::submit('Update Category', ['class'=>'btn btn-primary col-sm-6']) !!}
+        </div>
+
+        {!! Form::close() !!}
+
+
+
+
+
+        {!! Form::open(['method'=>'DELETE', 'action'=>['AdminCategoriesController@destroy', $category->id]]) !!}
+
+        <div class="form-group">
+            {!! Form::submit('Delete Category', ['class'=>'btn btn-danger col-sm-6']) !!}
         </div>
 
         {!! Form::close() !!}
@@ -25,7 +37,6 @@
     </div>
 
     <div class="col-sm-6">
-
 
 
 
